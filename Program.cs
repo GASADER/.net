7e4cae741cc5f78857c7d3c9461a7166b08f9f0a
josave <josave@odds.team>
2023-08-11 09:10:13 +0700

    Add: init

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+    namespace Dotnet
+    {
+        class Program{
+            public static void Main(string[] args)
+            {
+                PrintFunctions();
+            }
+
+            static void PrintFunctions(){
+                Console.WriteLine("Hello World");
+                Console.ReadKey();
+            }
+            
+        }
+    }
